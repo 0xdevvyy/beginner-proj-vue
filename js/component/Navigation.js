f8ee2dff498f3da63db1ad2724d6b54cd6396b74
0xdevvyy <pullhead45@gmail.com>
2025-12-09 15:31:23 +0800

    feat: adding a navigation header

---
+++ js/component/Navigation.js
@@ -0,0 +1,9 @@
+export default {
+    template: `
+        <header class="bg-gray-900 text-white py-4">
+            <nav class="container mx-auto flex flex-col items-center gap-4">
+                <h1 class="text-2xl font-bold tracking-wide">....</h1>
+            </nav> 
+        </header>
+    `
+}
